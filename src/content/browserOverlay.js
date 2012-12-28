57e510d1f64c0d51458f6d043d3dcb124ac3cfb1
Anders Lindmark <anders.lindmark@gmail.com>
2012-12-29 00:02:40 +0100

    Folder path now included

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -22,58 +22,70 @@ XULSchoolChrome.BrowserOverlay = {
   sayHello : function(aEvent) {
     //let stringBundle = document.getElementById("xulschoolhello-string-bundle");
     //let message = stringBundle.getString("xulschoolhello.greeting.label");
-
     //window.alert(message);
 
-    var bookmarks = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
-    var history = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
 
-    var query = history.getNewQuery();
-    window.alert("History: " + history);
-    // When more than one base-folder is specified, it seems that no subfolders are found, _AT ALL_
+	// When more than one base-folder is specified, it seems that no subfolders are found, _AT ALL_
     // So in order to know where a found bookmark lives, the query needs to be done on one base-folder
     // at a time, and then recursed through subdirectories, keeping a manual history of the path........
     //var folders = [bookmarks.toolbarFolder, bookmarks.bookmarksMenuFolder, bookmarks.unfiledBookmarksFolder];
     //query.setFolders(folders, folders.length);
 
-    query.setFolders([bookmarks.toolbarFolder], 1);
 
-    var options = history.getNewQueryOptions();
-    options.queryType = options.QUERY_TYPE_BOOKMARKS;
+    var bookmarks = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
+    var history = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
+
+    
     //window.alert("Pre query");
     //options.queryType = options.GROUP_BY_FOLDER;
     //window.alert("Post query");
-    var result = history.executeQuery(query, options);
 
-    var resultContainerNode = result.root;
+    function queryFolderRec(resultList, baseFolder, path) {
+	    var query = history.getNewQuery();
+	    query.setFolders([baseFolder], 1);
 
-    function queryFolderRec(resultList, folderNode) {
+	    var options = history.getNewQueryOptions();
+	    options.queryType = options.QUERY_TYPE_BOOKMARKS;
+
+	    var result = history.executeQuery(query, options);
+
+	    var folderNode = result.root;
+	    if (path == "") {
+	    	path = folderNode.title + "/";
+	    }
+	    else {
+	    	path = path + folderNode.title + "/";
+	    }
+
+	    //window.alert(path);
+
 		folderNode.containerOpen = true;
-		window.alert("History: " + history);
     
 	    for (var i = 0; i < folderNode.childCount; i++) {
 	    	var childNode = folderNode.getChild(i);
 	    	if (childNode.type == childNode.RESULT_TYPE_URI)
 	    	{
-				resultList.push(childNode);
+	    		var obj = {node: childNode, path: path};
+				resultList.push(obj);
+				//if (i > 5 && i < 10)
+				//	window.alert("obj.node: " + obj.node + ", obj.path: " + obj.path);
 	    	}
 	    	else if (childNode.type == childNode.RESULT_TYPE_FOLDER)
 	    	{
-	    		window.alert("Eureka!: " + childNode.parent.title);
+	    		queryFolderRec(resultList, childNode.itemId, path);
 	    	}
-	    	//if(i > 200) { break; }
 	    }
 	    folderNode.containerOpen = false;
     }
 	
 	var resultList = [];
-    queryFolderRec(resultList, resultContainerNode);
-
-    
+    queryFolderRec(resultList, bookmarks.toolbarFolder, "");
+    queryFolderRec(resultList, bookmarks.bookmarksMenuFolder, "");
+    queryFolderRec(resultList, bookmarks.unfiledBookmarksFolder, "");
 
     resultList.sort( function(a,b) {
-    	if (a.uri < b.uri) return -1;
-    	if (a.uri > b.uri) return 1;
+    	if (a.node.uri < b.node.uri) return -1;
+    	if (a.node.uri > b.node.uri) return 1;
     	return 0;
 	});
     // window.alert("Found " + resultList.length + " bookmarks");
@@ -84,19 +96,18 @@ XULSchoolChrome.BrowserOverlay = {
     	var thisBookmark = resultList[i];
     	var nextBookmark = resultList[i+1];
     	console.debug(dupcount + "/" + resultList.length);
-    	if (thisBookmark.uri == nextBookmark.uri) {
+    	if (thisBookmark.node.uri == nextBookmark.node.uri) {
     		dupcount++;
-    		console.log(thisBookmark.uri);
-    		if (!(thisBookmark.uri in dupList)) {
-    			dupList[thisBookmark.uri] = [];
+    		if (!(thisBookmark.node.uri in dupList)) {
+    			dupList[thisBookmark.node.uri] = [];
     			//window.alert("Created duplist[" + thisBookmark.uri + "]");
     		}
     		
-    		if (dupList[thisBookmark.uri].indexOf(thisBookmark) == -1) {
-    			dupList[thisBookmark.uri].push(thisBookmark);
+    		if (dupList[thisBookmark.node.uri].indexOf(thisBookmark) == -1) {
+    			dupList[thisBookmark.node.uri].push(thisBookmark);
     		}
-    		if (dupList[thisBookmark.uri].indexOf(nextBookmark) == -1) {
-    			dupList[thisBookmark.uri].push(nextBookmark);
+    		if (dupList[thisBookmark.node.uri].indexOf(nextBookmark) == -1) {
+    			dupList[thisBookmark.node.uri].push(nextBookmark);
     		}
     	}
     }
