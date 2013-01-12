7cf31367630c8381e80f5aa204a821cd9095f515
Anders Lindmark <anders.lindmark@gmail.com>
2013-01-12 01:18:08 +0100

    Clickable status-message + cleanup

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -6,27 +6,13 @@ if ("undefined" == typeof(XULBookmarkDup)) {
 };
 
 /**
- * Controls the browser overlay for the Hello World extension.
+ * Controls the browser overlay
  */
 XULBookmarkDup.BrowserOverlay = {
-  /**
-   * Says 'Hello' to the user.
-   */
-
   findDups : function(aEvent) {
-    //let stringBundle = document.getElementById("xulschoolhello-string-bundle");
-    //let message = stringBundle.getString("xulschoolhello.greeting.label");
-    //window.alert(message);
+  	var bookmarksService = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
+  	var historyService = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
 
-
-	// When more than one base-folder is specified, it seems that no subfolders are found, _AT ALL_
-    // So in order to know where a found bookmark lives, the query needs to be done on one base-folder
-    // at a time, and then recursed through subdirectories, keeping a manual history of the path........
-    //var folders = [bookmarks.toolbarFolder, bookmarks.bookmarksMenuFolder, bookmarks.unfiledBookmarksFolder];
-    //query.setFolders(folders, folders.length);
-	bookmarksService = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
-  	historyService = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
-
-    var win = window.openDialog("chrome://bookmarkdup/content/bookmarkdupwindow.xul", "bmdup", "chrome,width=615, height=300", {bmService: bookmarksService, hService: historyService});
+    window.openDialog("chrome://bookmarkdup/content/bookmarkdupwindow.xul", "bmdup", "chrome,width=615, height=300", {bmService: bookmarksService, hService: historyService});
   }
 };
