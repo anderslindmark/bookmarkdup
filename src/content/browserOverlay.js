ff7249c9a9bc13cb92e6c1ccf36f9655e32b22a6
Anders Lindmark <anders.lindmark@gmail.com>
2013-01-22 22:46:23 +0100

    Fixed bugs in the updating of the listboxes. Removed the Close button and moved the status bar to the same row as the buttons.

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -13,6 +13,7 @@ XULBookmarkDup.BrowserOverlay = {
   	var bookmarksService = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
   	var historyService = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsINavHistoryService);
 
-    window.openDialog("chrome://bookmarkdup/content/bookmarkdupwindow.xul", "bmdup", "chrome,width=615, height=300", {bmService: bookmarksService, hService: historyService});
+    window.openDialog("chrome://bookmarkdup/content/bookmarkdupwindow.xul", "bmdup", 
+    	"chrome=yes,resizable=yes", {bmService: bookmarksService, hService: historyService});
   }
 };
